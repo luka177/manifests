8df8e914399b291dbaab0f3a95833aae75a47944
kiam001 <blablatupfen@gmail.com>
2020-11-24 23:34:09 +0100

    remove nokia pl2 from cedric sync

---
+++ kiam001/plate2.mk
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<manifest>
+
+  <remote name="gerrit"
+          fetch="https://gerrit.kiam001.tk" />
+
+  <!--Device repos-->
+  <project path="device/nokia/PL2" name="kiam001/device_nokia_PL2" remote="github" revision="main" />
+  <project path="device/nokia/sdm660-common" name="NOKIA-DEVICE-TREE/device_nokia_sdm660-common" remote="github" revision="main" /> 
+  <project path="kernel/nokia/sdm660" name="NOKIA-DEVICE-TREE/kernel_nokia_sdm660" remote="github" revision="main" />
+  <project path="vendor/nokia" name="NOKIA-DEVICE-TREE/vendor_nokia" remote="github" revision="main" />
+
+  <!--Dependencies-->
+
+
+</manifest>
